0dc8ae1b440f8e73e13948c9b5c44af086cbd405
Priyanka <Priyanka@PRIYANKA-PC>
2017-03-26 01:53:41 +0530

    first commit

--- src/main/java/com/bp/micro/svc/bo/Runner.java
+++ src/main/java/com/bp/micro/svc/bo/Runner.java
@@ -5,7 +5,7 @@ import java.math.BigInteger;
 public class Runner {
   public static void main(String[] args) throws Exception {
     BigInteger first = new BigInteger("123456789123456789");
-    BigInteger second = new BigInteger("100000000000000000");
+    BigInteger second = new BigInteger("10000000000000");
 
     System.out.println(first.add(second));
     System.out.println(first.multiply(second));
